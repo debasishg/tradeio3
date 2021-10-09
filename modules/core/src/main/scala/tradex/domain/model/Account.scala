b98357dd9d57cb4b7c849b8b038156f448278c5a
Debasish Ghosh <ghosh.debasish@gmail.com>
2021-10-09 23:56:31 +0530

    Added more validations

--- modules/core/src/main/scala/tradex/domain/model/Account.scala
+++ modules/core/src/main/scala/tradex/domain/model/Account.scala
@@ -8,6 +8,9 @@ import cats.syntax.all.*
 
 import utils.Newtype
 
+// top level definition
+def today = LocalDateTime.now
+
 object account:
   enum AccountType(val entryName: String):
     case Trading extends AccountType("Trading")
@@ -25,6 +28,7 @@ object account:
       settlementCurrency: Option[Currency]
   )
 
+  // newtypes for AccountNo
   type AccountNo = String
   object AccountNo extends Newtype[String]
   extension (ano: AccountNo.Type)
@@ -33,6 +37,7 @@ object account:
         s"AccountNo cannot be more than 12 characters or less than 5 characters long".invalidNec
       else ano.validNec
 
+  // newtypes for AccountName
   type AccountName = String
   object AccountName extends Newtype[String]
   extension (aname: AccountName.Type)
@@ -42,6 +47,7 @@ object account:
       else aname.validNec
 
   object Account:
+    // smart constructors
     def tradingAccount(
         no: AccountNo.Type,
         name: AccountName.Type,
@@ -52,17 +58,108 @@ object account:
     ): ValidatedNec[String, Account] = {
       (
         no.validateNo,
-        name.validateName
-      ).mapN { (n, nm) =>
+        name.validateName,
+        validateOpenCloseDate(openDate.getOrElse(today), closeDate)
+      ).mapN { (n, nm, d) =>
         Account(
           n,
           nm,
-          openDate.get,
-          closeDate,
+          d._1,
+          d._2,
           AccountType.Trading,
           baseCcy,
           tradingCcy.some,
           None
         )
       }
+    }
+
+    def settlementAccount(
+        no: AccountNo.Type,
+        name: AccountName.Type,
+        openDate: Option[LocalDateTime],
+        closeDate: Option[LocalDateTime],
+        baseCcy: Currency,
+        settlementCcy: Currency
+    ): ValidatedNec[String, Account] = {
+      (
+        no.validateNo,
+        name.validateName,
+        validateOpenCloseDate(openDate.getOrElse(today), closeDate)
+      ).mapN { (n, nm, d) =>
+        Account(
+          n,
+          nm,
+          d._1,
+          d._2,
+          AccountType.Settlement,
+          baseCcy,
+          None,
+          settlementCcy.some
+        )
+      }
+    }
+
+    def tradingAndSettlementAccount(
+        no: AccountNo.Type,
+        name: AccountName.Type,
+        openDate: Option[LocalDateTime],
+        closeDate: Option[LocalDateTime],
+        baseCcy: Currency,
+        tradingCcy: Currency,
+        settlementCcy: Currency
+    ): ValidatedNec[String, Account] = {
+      (
+        no.validateNo,
+        name.validateName,
+        validateOpenCloseDate(openDate.getOrElse(today), closeDate)
+      ).mapN { (n, nm, d) =>
+        Account(
+          n,
+          nm,
+          d._1,
+          d._2,
+          AccountType.Both,
+          baseCcy,
+          tradingCcy.some,
+          settlementCcy.some
+        )
+      }
+    }
+
+    private def validateOpenCloseDate(
+        od: LocalDateTime,
+        cd: Option[LocalDateTime]
+    ): ValidatedNec[String, (LocalDateTime, Option[LocalDateTime])] =
+      cd.map { c =>
+        if (c isBefore od)
+          s"Close date [$c] cannot be earlier than open date [$od]".invalidNec
+        else (od, cd).validNec
+      }.getOrElse { (od, cd).validNec }
+
+    private def validateAccountAlreadyClosed(
+        a: Account
+    ): ValidatedNec[String, Account] = {
+      if (a.dateOfClose.isDefined)
+        s"Account ${a.no} is already closed".invalidNec
+      else a.validNec
+    }
+
+    private def validateCloseDate(
+        a: Account,
+        cd: LocalDateTime
+    ): ValidatedNec[String, LocalDateTime] = {
+      if (cd isBefore a.dateOfOpen)
+        s"Close date [$cd] cannot be earlier than open date [${a.dateOfOpen}]".invalidNec
+      else cd.validNec
+    }
+
+    def close(
+        a: Account,
+        closeDate: LocalDateTime
+    ): ValidatedNec[String, Account] = {
+      (validateAccountAlreadyClosed(a), validateCloseDate(a, closeDate))
+        .mapN { (acc, _) =>
+          acc.copy(dateOfClose = Some(closeDate))
+        }
     }
