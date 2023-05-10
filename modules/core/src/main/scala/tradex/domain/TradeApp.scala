a72e8276ac73e0c5e4f94f5a80830652872e2f66
Debasish Ghosh <dghosh@acm.org>
2023-05-10 22:56:01 +0530

    Integrating the full trade cycle in main app

--- modules/core/src/main/scala/tradex/domain/TradeApp.scala
+++ modules/core/src/main/scala/tradex/domain/TradeApp.scala
@@ -1,25 +1,27 @@
 package tradex.domain
 
 import zio.{ Scope, Task, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer }
+import zio.stream.{ ZPipeline, ZStream }
 import zio.interop.catz.*
 import cats.effect.std.Console
 import natchez.Trace.Implicits.noop
 import java.time.{ LocalDate, ZoneOffset }
 import java.util.UUID
+import java.nio.charset.StandardCharsets
 
 import config.AppConfig
 import resources.AppResources
 import model.user.UserId
 import repository.live.OrderRepositoryLive
-import service.TradingService
-import service.live.TradingServiceLive
+import repository.live.ExecutionRepositoryLive
+import service.{ ExchangeExecutionParsingService, FrontOfficeOrderParsingService, TradingService }
+import service.live.{ ExchangeExecutionParsingServiceLive, FrontOfficeOrderParsingServiceLive, TradingServiceLive }
 
 object TradeApp extends ZIOAppDefault:
 
   override def run: ZIO[Any & (ZIOAppArgs & Scope), Any, Any] =
     val setupDB = for {
       dbConf <- ZIO.serviceWith[AppConfig](_.postgreSQL)
-      // _      <- FlywayMigration.migrate(dbConf)
     } yield ()
 
     given Console[Task] = Console.make[Task]
@@ -31,13 +33,30 @@ object TradeApp extends ZIOAppDefault:
       yield res
     )
 
-    val live: ZLayer[Any, Throwable, TradingService] = ZLayer
-      .make[TradingService](
+    val live: ZLayer[
+      Any,
+      Throwable,
+      TradingService with FrontOfficeOrderParsingService with ExchangeExecutionParsingService
+    ] = ZLayer
+      .make[TradingService with FrontOfficeOrderParsingService with ExchangeExecutionParsingService](
         TradingServiceLive.layer,
         OrderRepositoryLive.layer,
+        ExecutionRepositoryLive.layer,
+        FrontOfficeOrderParsingServiceLive.layer,
+        ExchangeExecutionParsingServiceLive.layer,
         appResourcesL.project(_.postgres),
         appResourcesL,
         config.appConfig
+      )
+
+    def reader(name: String) = ZIO
+      .scoped(
+        ZIO.fromAutoCloseable(
+          ZStream
+            .fromResource(name)
+            .via(ZPipeline.decodeCharsWith(StandardCharsets.UTF_8))
+            .toReader
+        )
       )
 
     val genTrades = for
@@ -48,5 +67,10 @@ object TradeApp extends ZIOAppDefault:
       _       <- ZIO.logInfo(s"Done generating ${trades.size} trades")
     yield ()
 
+    val tradingCycle = (reader("order.csv") <*> reader("execution.csv")).flatMap { case (order, execution) =>
+      (ZIO.serviceWithZIO[FrontOfficeOrderParsingService](_.parse(order)) <&>
+        ZIO.serviceWithZIO[ExchangeExecutionParsingService](_.parse(execution))).map(_ => genTrades)
+    }
+
     setupDB.provide(config.appConfig)
-      *> genTrades.provide(live)
+      *> tradingCycle.provide(live)
