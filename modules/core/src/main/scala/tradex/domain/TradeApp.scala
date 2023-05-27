0a33428782372c2d2fac1aac07667c2c9fbc3c29
Debasish Ghosh <dghosh@acm.org>
2023-05-27 23:59:43 +0530

    Refactoring

--- modules/core/src/main/scala/tradex/domain/TradeApp.scala
+++ modules/core/src/main/scala/tradex/domain/TradeApp.scala
@@ -18,74 +18,79 @@ import service.{ ExchangeExecutionParsingService, FrontOfficeOrderParsingService
 import service.live.{ ExchangeExecutionParsingServiceLive, FrontOfficeOrderParsingServiceLive, TradingServiceLive }
 
 object TradeApp extends ZIOAppDefault:
+  import TradeAppConfig.*
+  import TradeAppComponents.*
 
   override def run: ZIO[Any & (ZIOAppArgs & Scope), Any, Any] =
-    val setupDB = for {
-      dbConf <- ZIO.serviceWith[AppConfig](_.postgreSQL)
-    } yield ()
-
-    given Console[Task] = Console.make[Task]
-
-    val appResourcesL: ZLayer[AppConfig, Throwable, AppResources] = ZLayer.scoped(
-      for
-        config <- ZIO.service[AppConfig]
-        res    <- AppResources.make(config).toScopedZIO
-      yield res
-    )
-
-    val live: ZLayer[
-      Any,
-      Throwable,
-      TradingService & FrontOfficeOrderParsingService & ExchangeExecutionParsingService
-    ] = ZLayer
-      .make[TradingService & FrontOfficeOrderParsingService & ExchangeExecutionParsingService](
-        TradingServiceLive.layer,
-        OrderRepositoryLive.layer,
-        ExecutionRepositoryLive.layer,
-        FrontOfficeOrderParsingServiceLive.layer,
-        ExchangeExecutionParsingServiceLive.layer,
-        appResourcesL.project(_.postgres),
-        appResourcesL,
-        config.appConfig
-      )
-
-    val genTrades = for
-      now  <- zio.Clock.instant
-      uuid <- zio.Random.nextUUID
-      trades <- ZIO
-        .serviceWithZIO[TradingService](
-          _.generateTrades(LocalDate.ofInstant(now, ZoneOffset.UTC), UserId(uuid)).runCollect
-        )
-      _ <- ZIO.logInfo(s"Done generating ${trades.size} trades")
-    yield ()
-
-    val parseFOrders =
-      ZIO
-        .scoped(
-          ZStream
-            .fromResource("forders.csv")
-            .via(ZPipeline.decodeCharsWith(StandardCharsets.UTF_8))
-            .toReader
-            .flatMap(reader => ZIO.serviceWithZIO[FrontOfficeOrderParsingService](_.parse(reader)))
-        )
-
-    val parseExchangeExecutions =
-      ZIO
-        .scoped(
-          ZStream
-            .fromResource("executions.csv")
-            .via(ZPipeline.decodeCharsWith(StandardCharsets.UTF_8))
-            .toReader
-            .flatMap(reader => ZIO.serviceWithZIO[ExchangeExecutionParsingService](_.parse(reader)))
-        )
 
     val tradingCycle =
       ZIO.logInfo(s"Parsing front office orders ..") *>
-        parseFOrders *>
+        parseFrontOfficeOrders *>
         ZIO.logInfo(s"Parsing exchange executions ..") *>
         parseExchangeExecutions *>
         ZIO.logInfo(s"Generating trades ..") *>
-        genTrades
+        generateTrades
 
     setupDB.provide(config.appConfig)
       *> tradingCycle.provide(live)
+
+object TradeAppConfig:
+  val setupDB =
+    for dbConf <- ZIO.serviceWith[AppConfig](_.postgreSQL)
+    yield ()
+
+  given Console[Task] = Console.make[Task]
+
+  val appResourcesL: ZLayer[AppConfig, Throwable, AppResources] = ZLayer.scoped(
+    for
+      config <- ZIO.service[AppConfig]
+      res    <- AppResources.make(config).toScopedZIO
+    yield res
+  )
+
+  val live: ZLayer[
+    Any,
+    Throwable,
+    TradingService & FrontOfficeOrderParsingService & ExchangeExecutionParsingService
+  ] = ZLayer
+    .make[TradingService & FrontOfficeOrderParsingService & ExchangeExecutionParsingService](
+      TradingServiceLive.layer,
+      OrderRepositoryLive.layer,
+      ExecutionRepositoryLive.layer,
+      FrontOfficeOrderParsingServiceLive.layer,
+      ExchangeExecutionParsingServiceLive.layer,
+      appResourcesL.project(_.postgres),
+      appResourcesL,
+      config.appConfig
+    )
+
+object TradeAppComponents:
+  val generateTrades = for
+    now  <- zio.Clock.instant
+    uuid <- zio.Random.nextUUID
+    trades <- ZIO
+      .serviceWithZIO[TradingService](
+        _.generateTrades(LocalDate.ofInstant(now, ZoneOffset.UTC), UserId(uuid)).runCollect
+      )
+    _ <- ZIO.logInfo(s"Done generating ${trades.size} trades")
+  yield ()
+
+  def reader(name: String) =
+    ZStream
+      .fromResource(name)
+      .via(ZPipeline.decodeCharsWith(StandardCharsets.UTF_8))
+      .toReader
+
+  val parseFrontOfficeOrders =
+    ZIO
+      .scoped(
+        reader("forders.csv")
+          .flatMap(reader => ZIO.serviceWithZIO[FrontOfficeOrderParsingService](_.parse(reader)))
+      )
+
+  val parseExchangeExecutions =
+    ZIO
+      .scoped(
+        reader("executions.csv")
+          .flatMap(reader => ZIO.serviceWithZIO[ExchangeExecutionParsingService](_.parse(reader)))
+      )
